ff82ac4340133e28e1a44aca01ccaf3c7dad3e70
Luis Perez Mosteiro <lperez@ntb-158.d01.keplertech.com>
2020-01-16 16:48:50 -0300

    Sincronizando Api para mostrar Array de elementos en la vista

---
+++ src/ItemDetail.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function Shop() {
+  useEffect(() => {},[]);
+
+  const [item, setItem] = useState({});
+
+  return (
+    <div>
+        <h1>Item</h1>
+    </div>
+  );
+}
+
+export default Shop;
--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  },[]);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+      const data = await fetch(
+          'https://www.eventbriteapi.com/v3/categories/?token=RGZSQ3RSRKJUFHU2CXYW&locale=es_ES'      
+      );
+
+      const items = await data.json();
+      console.log(items.categories);
+      setItems(items.categories);
+
+  };
+
   return (
     <div>
       <h1>Shop Page</h1>
+      {items.map(item => (
+        <h1 key={item.id}>
+           <Link to={`/shop/${item.id}`}>{item.name}</Link>
+        </h1>
+      ))}
     </div>
   );
 }
